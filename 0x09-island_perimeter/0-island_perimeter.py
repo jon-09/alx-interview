1eed134569f8f3814ecab824b26d906ea85f1b46
jon-09 <achola.jon@yahoo.com>
2023-04-01 11:58:10 +0300

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,26 +5,16 @@
 
 def island_perimeter(grid):
     """ determines perimeter of described island """
-    visited = set()
-
-    if not grid:
-        return 0
-
-    def dfs(i, j):
-        """ deep first search """
-        if (i, j) in visited:
-            return 0
-        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
-            return 1
-        visited.add((i, j))
-        result = dfs(i, j + 1)
-        result += dfs(i, j - 1)
-        result += dfs(i + 1, j)
-        result += dfs(1 - 1, j)
-        return result
+    p = 0
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            """ dfs for land """
-            if grid[i][j]:
-                return dfs(i, j)
-    return 0
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p

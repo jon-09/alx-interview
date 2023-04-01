01037474931eb544bfc3bd7a4c3ea591251ba9ec
jon-09 <achola.jon@yahoo.com>
2023-04-01 08:35:28 +0300

    0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+"""rotate 2d matrix """
+
 def rotate_2d_matrix(matrix):
+
+	""" rotate n x n matrix 90 degrees clockwise
+	"""
 
 l = len(matrix)
 for layer in range(n // 2):

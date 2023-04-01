8f6a25c6564cab7b14101708eda5df07cbb5165b
jon-09 <achola.jon@yahoo.com>
2023-04-01 10:00:24 +0300

    README.md

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,19 +3,18 @@
 
 import sys
 
+
 def makeChange(coins, total):
-	""" return fewest number of coins needed to meet total """
-	if (total == 0):
-		return 0
+    """ return fewest number of coins needed to meet total """
+    if (total == 0):
+        return 0
 
-	count = sys.maxsize
+    count = sys.maxsize
 
-	for i in range(0, len(coins)):
-		if (coins[i] <= total):
-			prevCount = makeChange(coins, total - coins[i])
-			if (prevCount + 1 < count and prevCount != sys.maxsize):
-				count = prevCount + 1
+    for i in range(0, len(coins)):
+        if (coins[i] <= total):
+            prevCount = makeChange(coins, total - coins[i])
+            if (prevCount + 1 < count and prevCount != sys.maxsize):
+                count = prevCount + 1
 
-
-	return count
-
+    return count
